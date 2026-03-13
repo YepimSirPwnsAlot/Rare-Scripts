cb60212611eaed6d7f51066f7e0ef23369c1f215
YepImNythic <guest45542008@gmail.com>
2026-03-13 00:23:57 -0400

    Update and rename ro-xploit tinyhub.lua to ro-xploit tinyhub.cpp

---
+++ ro-xploit tinyhub.cpp
@@ -0,0 +1,8 @@
+--// Im not leaking this but heres some free C++ print i guess, also i turned it from lua to cpp for the cpp syntax highlighting
+#include <iostream>
+using namespace std;
+
+int main() {
+	std::cout << "uh idk but your not getting the code for ro-xploit tiny hub"
+	reutrn 0;
+}
